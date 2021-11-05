2fc33470cc9b2ee2cf6b1afc6bccaea91c0fefe6
Kristoffer Klevjer <kristoffer@klevjer.no>
2021-11-05 18:32:08 +0100

    OT is now overall time on page

--- src/functions/recoding.time.R
+++ src/functions/recoding.time.R
@@ -5,7 +5,7 @@
 
 # Time spent per page ------------------------------------------------------------------------------
 # Input: Columns of first click, last click, page submit and click-count
-# Output: Overall time on page, time from first to last action, time from last action to submit,
+# Output: Overall time on page, time from first action to submit, time from last action to submit,
 #         Number of clicks above minimum (e.g., 15items, 17 clicks = 2 clicks above minimum)
 # Use: df[,c(x1:x4)] <- recodeTime(x = df[,c(x1:x4])
 recodeTime <- function(x) {
@@ -14,9 +14,9 @@ recodeTime <- function(x) {
     x[,i] <- as.numeric(x[,i])
   }
   x <- x
-  xOT <- (x[,3] - x[,1])
-  xFL <- (x[,2] - x[,1])
+  xOT <- (x[,3])
+  xFS <- (x[,3] - x[,1])
   xLS <- (x[,3] - x[,2])
   xCM <- (x[,4] - min(x[,4], na.rm = TRUE))
-  x <- cbind(xOT, xFL, xLS, xCM)
+  x <- cbind(xOT, xFS, xLS, xCM)
 }
