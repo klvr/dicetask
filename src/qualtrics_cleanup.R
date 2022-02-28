973a7ce8b2db10545f73e078b4a16fa1c358e4d2
Kristoffer Klevjer <klevjer@gmail.com>
2022-02-28 12:06:05 +0100

    Added students_2 data to cleanup script

--- src/qualtrics_cleanup.R
+++ src/qualtrics_cleanup.R
@@ -12,8 +12,7 @@
 rm(list = ls())
 
 # Set up meta file to capture data removal
-metaProlific <- NULL
-metaStudents <- NULL
+metaQualtrics <- NULL
 
 # 01 Load raw data ---------------------------------------------------------------------------------
 
@@ -36,19 +35,20 @@ qualtrics_prolific <- qualtrics_prolific[-1,]
 consentProlific <- qualtrics_prolific$QID74 != 
   "No, I am not willing to participate; one or more statement does not apply to me."
 qualtrics_prolific <- qualtrics_prolific[consentProlific,]
-metaProlific <- rbind(metaProlific, c(sum(consentProlific == FALSE), "no consent"))
+metaQualtrics <- rbind(metaQualtrics, c(sum(consentProlific == FALSE), "no consent", "Prolific"))
 
 # Remove non-completed entries
 completionProlific <- qualtrics_prolific$finished == "True"
 qualtrics_prolific <- qualtrics_prolific[completionProlific,]
-metaProlific <- rbind(metaProlific, c(sum(completionProlific== FALSE), "not completed"))
+metaQualtrics <- rbind(metaQualtrics, c(sum(completionProlific== FALSE), "not completed",
+                                        "Prolific"))
 
-# 03 Prepare Students data ------------------------------------------------------------------------
+# 03 Prepare Students_1 data ------------------------------------------------------------------------
 
 # Remove item description row
-qualtrics_students_1 <- qualtrics_students_1[-1,]
-qualtrics_students_2 <- qualtrics_students_2[-1,]
-qualtrics_students_3 <- qualtrics_students_3[-1,]
+qualtrics_students_1 <- qualtrics_students_1_a[-1,]
+qualtrics_students_2 <- qualtrics_students_1_b[-1,]
+qualtrics_students_3 <- qualtrics_students_1_c[-1,]
 
 # Remove pilot/technical-pilot data
 pilotStudents <- qualtrics_students_1$distributionChannel == "preview"
@@ -60,13 +60,13 @@ qualtrics_students_2 <- qualtrics_students_2[!pilotStudents,]
 consentStudents <- qualtrics_students_1$QID51 ==
   "I am willing to participate in the research project."
 qualtrics_students_1 <- qualtrics_students_1[consentStudents,]
-metaStudents <- rbind(metaStudents, c(sum(consentStudents == FALSE), "no consent"))
+metaQualtrics <- rbind(metaQualtrics, c(sum(consentStudents == FALSE), "no consent", "students_1"))
 
 # Remove (self-)terminated runs (due to being on mobile)
 mobileStudents <- qualtrics_students_1$QID113 == 
   "Yes, and I'm not using Safari"
 qualtrics_students_1 <- qualtrics_students_1[mobileStudents,]
-metaStudents <- rbind(metaStudents, c(sum(mobileStudents == FALSE), "on mobile"))
+metaQualtrics <- rbind(metaQualtrics, c(sum(mobileStudents == FALSE), "on mobile", "students_1"))
 
 # Remove aborted runs (all data missing from ID and out)
 abortedStudents <- as.numeric(qualtrics_students_1$QID58_TEXT) > 1
@@ -77,30 +77,78 @@ qualtrics_students_1 <- qualtrics_students_1[-c(1:4),]
 qualtrics_students_2 <- qualtrics_students_2[-c(1:3),]
 qualtrics_students_3 <- qualtrics_students_3[-1,]
 
+# 04 Prepare Students_2 data -----------------------------------------------------------------------
+
+# Remove item description rows
+qualtrics_students_2_a <- qualtrics_students_2_a[-c(1,2),]
+qualtrics_students_2_b <- qualtrics_students_2_b[-c(1,2),]
+qualtrics_students_2_c <- qualtrics_students_2_c[-c(1,2),]
+qualtrics_students_2_nfc <- qualtrics_students_2_nfc[-c(1,2),]
+
+# Remove pilot/technical-pilot data
+pilotStudents2 <- qualtrics_students_2_a$DistributionChannel == "preview"
+qualtrics_students_2_a <- qualtrics_students_2_a[!pilotStudents2,]
+pilotStudents2 <- qualtrics_students_2_b$DistributionChannel == "preview"
+qualtrics_students_2_b <- qualtrics_students_2_b[!pilotStudents2,]
+pilotStudents2 <- qualtrics_students_2_c$DistributionChannel == "preview"
+qualtrics_students_2_c <- qualtrics_students_2_c[!pilotStudents2,]
+
+# Remove non-consent participants
+consentStudents2 <- qualtrics_students_2_a$consent2.== 
+  "I am willing to participate in the research project."
+qualtrics_students_2_a <- qualtrics_students_2_a[consentStudents2,]
+metaQualtrics <- rbind(metaQualtrics, c(sum(consentStudents2 == FALSE), "no consent", "students_2"))
+
+# Remove (self-)terminated runs (due to being on mobile)
+mobileStudents2 <- qualtrics_students_2_a$browser1. == "Yes, and I'm not using Safari"
+qualtrics_students_2_a <- qualtrics_students_2_a[mobileStudents2,]
+metaQualtrics <- rbind(metaQualtrics, c(sum(mobileStudents2== FALSE), "on mobile", "students_2"))
+
+# Remove aborted runs (all data missing from ID and out)
+abortedStudents2 <- qualtrics_students_2_a$FL_5_DO > 1
+qualtrics_students_2_a <- qualtrics_students_2_a[abortedStudents2,]
+
+# Make sure ID's are identical across all ID catching variables
+## If not, set up a data-frame for matching
+sameID <- cbind(qualtrics_students_2_a[,c(13,16,17,18)], 
+                qualtrics_students_2_a[,13] == qualtrics_students_2_a[,16], 
+                qualtrics_students_2_a[,16] == qualtrics_students_2_a[,17], 
+                qualtrics_students_2_a[,17] == qualtrics_students_2_a[,18])
+## Manual inspection gives two non-identical instances:
+### '6519' responded 'okay' rather than repeating ID, not change needed.
+### '6783' responded '1867' in one instance. Look for this ID (1867) in other tasks
+
 # 04 Removal of unnecessary & empty variables ------------------------------------------------------
 
 qualtrics_prolific <- qualtrics_prolific[,-c(1:11,14:16)]
 qualtrics_students_1 <- qualtrics_students_1[,-c(1:16,19:21,26,27,309:351,356,357)]
 qualtrics_students_2 <- qualtrics_students_2[,-c(1:30,333:353)]
 qualtrics_students_3 <- qualtrics_students_3[,-c(1:17)]
+qualtrics_students_2_a <- qualtrics_students_2_a[,-c(1:12,16:20)]
+qualtrics_students_2_b <- qualtrics_students_2_b[,-c(1:10)]
+qualtrics_students_2_c <- qualtrics_students_2_c[,-c(1:10)]
+
+# Remove all variables but NFC from qualtrics_students_2_nfc
+## This data was collected during a seperate experiment from someone else
+qualtrics_students_2_nfc <- qualtrics_students_2_nfc[,c(13,93:110)]
 
 # 05 Set-up meta-data files ------------------------------------------------------------------------
 
-metaProlific <- as.data.frame(metaProlific)
-metaStudents <- as.data.frame(metaStudents)
-colnames(metaProlific) <- c("removed", "reason")
-colnames(metaStudents) <- c("removed", "reason")
+metaQualtrics <- as.data.frame(metaQualtrics)
+colnames(metaQualtrics) <- c("N affected", "Reason", "Sample pool")
 
 # 06 Saving of temp-data and meta-data -------------------------------------------------------------
 
 # Saving temp files
-write.csv(qualtrics_prolific, file = "data/temp/qualtrics_prolific.csv", row.names = FALSE)
-write.csv(qualtrics_students_1, file = "data/temp/qualtrics_students_1.csv", row.names = FALSE)
-write.csv(qualtrics_students_2, file = "data/temp/qualtrics_students_2.csv", row.names = FALSE)
-write.csv(qualtrics_students_3, file = "data/temp/qualtrics_students_3.csv", row.names = FALSE)
+write.csv(qualtrics_prolific, file = "data/temp/qualtrics_prolific_temp.csv", row.names = FALSE)
+write.csv(qualtrics_students_1, file = "data/temp/qualtrics_students_1_a_temp.csv", row.names = FALSE)
+write.csv(qualtrics_students_2, file = "data/temp/qualtrics_students_1_b_temp.csv", row.names = FALSE)
+write.csv(qualtrics_students_3, file = "data/temp/qualtrics_students_1_c_temp.csv", row.names = FALSE)
+write.csv(qualtrics_students_2_a, file = "data/temp/qualtrics_students_2_a_temp.csv", row.names = FALSE)
+write.csv(qualtrics_students_2_b, file = "data/temp/qualtrics_students_2_b_temp.csv", row.names = FALSE)
+write.csv(qualtrics_students_2_c, file = "data/temp/qualtrics_students_2_c_temp.csv", row.names = FALSE)
+write.csv(qualtrics_students_2_nfc, file = "data/temp/qualtrics_students_2_nfc_temp.csv", row.names = FALSE)
 
 # Saving meta files
-write.csv(metaProlific, file = "data/processed/meta_prolific.csv", row.names = FALSE)
-write.csv(metaStudents, file = "data/processed/meta_students.csv", row.names = FALSE)
-write.csv(metaProlific, file = "data/temp/meta_prolific.csv", row.names = FALSE)
-write.csv(metaStudents, file = "data/temp/meta_students.csv", row.names = FALSE)
+write.csv(metaQualtrics, file = "data/processed/meta_qualtrics-cleanup.csv", row.names = FALSE)
+write.csv(metaQualtrics, file = "data/temp/meta_qualtrics.csv", row.names = FALSE)
